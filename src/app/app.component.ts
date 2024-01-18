ecec014a92f7b1ffcbf2cd4b0e014a328f7de18f
Jairo Martinez <ocrensys@gmail.com>
2024-01-18 16:07:19 -0600

    style(config): :art: format and lint files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'ims-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+	selector: 'ims-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.sass'
 })
-export class AppComponent {
-}
+export class AppComponent {}
